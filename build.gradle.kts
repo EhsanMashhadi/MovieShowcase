77e7a7dd60d0698eec26fd39995ee991a87fc8b6
EM <ehsan.mashhadi@gmail.com>
2025-05-25 11:32:03 -0600

    Bump dependencies version, Migrate to KSP (#3)
    Co-authored-by: Ehsan <6213824+EhsanMashhadi@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
-    id("com.google.dagger.hilt.android") version "2.56" apply false
-    kotlin("kapt") version "1.9.21" apply false
-
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt) apply false
 }
